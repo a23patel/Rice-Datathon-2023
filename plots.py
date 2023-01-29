00fdfc491b4d8f8f08b2c515373d6d6fde6530cf
Abraar Patel <87881362+a23patel@users.noreply.github.com>
2023-01-29 00:12:11 -0600

    Update plots.py

--- plots.py
+++ plots.py
@@ -6,8 +6,29 @@ import plotly.io as pio
 from urllib.request import urlopen
 import json
 
-def export_plotly_to_png(fig, filename):
+def export_plotly_to_png(fig, filename):    # converting plotly graphs to png files
     pio.write_image(fig, filename, format='png')
+    
+def create_bar_plot(dataframe, x, y, color, width, height, title_name='2010 Populations per State):
+    fig = px.bar(newdf, x = 'ABBR', y = 'POPEST2010_CIV', color='ABBR',  width=1000, height=650)
+    fig.update_layout(
+    title={
+        'text': title_name,
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+    xaxis_title = "States",
+    yaxis_title = "Populations",
+    showlegend=False
+    )
+    fig.show()
+    export_plotly_to_png(fig, "fig1.png")
+    return fig
+
+def pop2015_bar():
+    fig = create_bar_plot(newdf, 'ABBR', 'POPEST2015_CIV', 'ABBR', 1000, 650, '2015 Populations per State')
+                    
 
 def create_county_map(dataframe, county_column, var_column, cmap='Viridis'):
     counties = None
@@ -24,6 +45,7 @@ def create_county_map(dataframe, county_column, var_column, cmap='Viridis'):
                           )
 
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    export_plotly_to_png(fig, "fig4.png")
     return fig
 
 def create_state_map(dataframe, state_column, var_column, cmap='Viridis'):
@@ -40,19 +62,11 @@ def create_state_map(dataframe, state_column, var_column, cmap='Viridis'):
                           )
 
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    export_plotly_to_png(fig, "fig5.png")
     return fig
 
-def census_2010_pop(dataframe, state_column, var_column, cmap='Viridis'):
-    fig = px.choropleth(newdf, geojson = counties, 
-                           locations='state_column',
-                           locationmode="USA-states", color='var_column',
-                           color_continuous_scale=cmap,
-                           scope="usa",
-                           labels={'State Code':'States'}
-                          )
-
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    fig.show()
+def census_2010_map():
+    fig = create_state_map(newdf, 'State', 'ESTBASE2010_CIV', cmap='Viridis')
     export_plotly_to_png(fig, "fig6.png")
     return fig
     
