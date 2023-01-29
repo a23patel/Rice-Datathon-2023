b8bcf3e4a28582ff6cafc1bd2f10a4c4e555480a
Michael Moorman <michael.l.moorman@protonmail.com>
2023-01-28 19:01:54 -0600

    Fixed plot handling on counties

--- plots.py
+++ plots.py
@@ -11,7 +11,10 @@ def create_county_map(dataframe, county_column, var_column, cmap='Viridis'):
     with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
         counties = json.load(response)
 
-    fig = px.choropleth(dataframe, geojson = counties, locations=county_column, color=var_column,
+    sites_county_vc = dataframe[county_column].value_counts()
+    sites_county_vc_df = pd.DataFrame({'County': sites_county_vc.index, 'Count':sites_county_vc})
+
+    fig = px.choropleth(sites_county_vc_df, geojson = counties, locations='County', color='Count',
                            color_continuous_scale=cmap,
                            scope="usa",
                            labels={'State Code':'States'}
