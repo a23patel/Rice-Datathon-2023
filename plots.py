451b4f53ebe9b6cd83584f7ef7b95001d1479dec
aadproj3570 <104705067+aadproj3570@users.noreply.github.com>
2023-01-29 00:14:43 -0600

    Update plots.py

--- plots.py
+++ plots.py
@@ -3,11 +3,42 @@ import pandas as pd
 import plotly.express as px
 import seaborn as sns
 import plotly.io as pio
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from urllib.request import urlopen
 import json
 
 def export_plotly_to_png(fig, filename):
     pio.write_image(fig, filename, format='png')
+
+def population2010States(dataframe, state_column, var_column, cmap='Viridis'):
+    fig = px.bar(newdf, x = 'ABBR', y = 'POPEST2010_CIV', color='ABBR',  width=1000, height=650)
+    fig.update_layout(title={'text': "2010 Populations per State",'y':0.9,'x':0.5,'xanchor': 'center','yanchor': 'top'}, xaxis_title = "States", yaxis_title = "Populations", 
+    showlegend=False)
+    fig.show()
+    return fig
+
+def population2015States(dataframe, state_column, var_column, cmap='Viridis'):
+    fig = px.bar(newdf, x = 'ABBR', y = 'POPEST2015_CIV', color='ABBR',  width=1000, height=650)
+    fig.update_layout(title={'text': "2010 Populations per State",'y':0.9,'x':0.5,'xanchor': 'center','yanchor': 'top'}, xaxis_title = "States", yaxis_title = "Populations", 
+    showlegend=False)
+    fig.show()
+    return fig
+
+def population2019States(dataframe, state_column, var_column, cmap='Viridis'):
+    fig = px.bar(newdf, x = 'ABBR', y = 'POPEST2019_CIV', color='ABBR',  width=1000, height=650)
+    fig.update_layout(title={'text': "2019 Populations per State",'y':0.9,'x':0.5,'xanchor': 'center','yanchor': 'top'}, xaxis_title = "States", yaxis_title = "Populations", 
+    showlegend=False)
+    fig.show()
+    return fig
+
+def sitesperState(dataframe, county_column, var_column, cmap='Viridis'):
+    sites_df = parse_sites(collab_dir+'/beginner.txt', include_PR=False)
+    fig = px.histogram(sites_df, x='State Code', color='State Code', width=1000, height=650)
+    fig.update_layout(title={'text': "Number of sites per State", 'y':0.9, 'x':0.5, 'xanchor': 'center','yanchor': 'top'}, xaxis_title = "States", showlegend = False)
+    fig.show()
+    return fig
 
 def create_county_map(dataframe, county_column, var_column, cmap='Viridis'):
     counties = None
